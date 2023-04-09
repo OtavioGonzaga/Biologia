19f953095a3ca29a81ea3a9d1334ed8cc267ccbf
Otavio Gonzaga <otaviolgonzaga@gmail.com>
2023-04-09 18:55:09 -0300

    Seleção de gatos

--- script/script.js
+++ script/script.js
@@ -1,10 +1,17 @@
 function borderCat(img) {
-    let cats = document.querySelectorAll('.blue')
-    if ((img.style.border).includes('rgb')) {
-        img.style.border = '8px solid white'
+    const section = document.querySelector('section')
+    if (img.outerHTML.includes('blue')) {
         img.classList.toggle('blue')
-    } else if (cats.length < 2) {
-        img.style.border = '8px solid #4c8bf5'
+    } else if (document.querySelectorAll('.blue').length < 2) {
         img.classList.toggle('blue')
     }
+    if (document.querySelectorAll('.blue').length == 2 && !document.querySelector('button')) {
+        section.innerHTML += '<button onclick="cruzar()">Cruzar</button>'
+    } else if (document.querySelector('button')){
+        const btn = document.querySelector('button')
+        btn.outerHTML = null 
+    }
+}
+function cruzar() {
+    console.log(document.querySelector('.cat').innerText.includes('A'))
 }
