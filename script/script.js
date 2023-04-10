3a16693ab3adc75780755b829bab5ab110863be8
Otavio Gonzaga <otaviolgonzaga@gmail.com>
2023-04-09 22:02:03 -0300

    Geração incompleta

--- script/script.js
+++ script/script.js
@@ -1,17 +1,27 @@
+const blackCat = 'img/black_cat_green_background.jpeg'
+const hetCat = 'img/black_cat_blue_background.jpeg'
+const whiteCat = 'img/white_cat_green_background.jpeg'
 function borderCat(img) {
-    const section = document.querySelector('section')
-    if (img.outerHTML.includes('blue')) {
-        img.classList.toggle('blue')
-    } else if (document.querySelectorAll('.blue').length < 2) {
-        img.classList.toggle('blue')
+    if (img.outerHTML.includes('selected')) {
+        img.classList.remove('selected')
+    } else if (document.querySelectorAll('.selected').length < 2) {
+        img.classList.add('selected')
     }
-    if (document.querySelectorAll('.blue').length == 2 && !document.querySelector('button')) {
-        section.innerHTML += '<button onclick="cruzar()">Cruzar</button>'
-    } else if (document.querySelector('button')){
-        const btn = document.querySelector('button')
-        btn.outerHTML = null 
+    if (document.querySelectorAll('.selected').length == 2) {
+        document.querySelector('button').outerHTML = '<button onclick="cruzar()"><i class="bi bi-star"></i>  Cruzar</button>'
+    } else if (!document.querySelector('button').outerHTML.includes('disabled')) {
+        document.querySelector('button').outerHTML = '<button class="disabled"><i class="bi bi-star"></i>  Cruzar</button>'
     }
 }
 function cruzar() {
-    console.log(document.querySelector('.cat').innerText.includes('A'))
+    var cats = document.querySelectorAll('.selected')
+    if (cats[0].innerText == 'AA' && cats[1].innerText == 'AA') var child = {gen: 'AA', color: blackCat}
+    if (cats[0].innerText == 'AA' && cats[1].innerText == 'aa') var child = {gen: 'Aa', color: hetCat}
+    if (cats[0].innerText == 'aa' && cats[1].innerText == 'aa') var child = {gen: 'aa', color: whiteCat}
+    document.querySelector('section').innerHTML += `<figure class="cat" onclick="borderCat(this)"><img src="${child.color}"><figcaption>${child.gen}</figcaption></figure>`
+    document.querySelector('button').outerHTML = '<button class="disabled"><i class="bi bi-star"></i>  Cruzar</button>'
+    document.querySelectorAll('.selected').forEach(e => e.classList.remove('selected'))
+}
+function reload() {
+    location.reload()
 }
